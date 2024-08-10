4edb207c3807fb0d04b6871699dcadf1a1852183
Saydva <kacurikjozef@gmail.com>
2024-08-11 00:22:54 +0200

    builded

---
+++ dist/index.17e34e62.js
@@ -0,0 +1,2 @@
+let e,t;e=document.querySelector("#search"),t=document.querySelector("#dtext"),e.addEventListener("keyup",n=>{n.preventDefault(),e.value?t.innerText=e.value:t.innerText="debug"});
+//# sourceMappingURL=index.17e34e62.js.map
---
+++ dist/index.1e176154.js
@@ -0,0 +1,2 @@
+let e=document.querySelector("#search"),t=document.querySelector("#dtext");e.addEventListener("keyup",n=>{n.preventDefault(),e.value?t.innerText=e.value:t.innerText="debug"});
+//# sourceMappingURL=index.1e176154.js.map
